9f2da39ae37a0d2636992c1778fc054dc8e0bb3c
Piet Ubrich <ubrich@inray.de>
2017-05-29 19:32:26 +0200

    - Added core library

---
+++ FileSystemBackup.Core/Encryption/Encryption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemBackup.Core.Encryption
+{
+    /// <summary>
+    /// Provides access to all implemented encryptions.
+    /// </summary>
+    public static class Encryption
+    {
+#pragma warning disable S125 // Sections of code should not be "commented out"
+        // TODO: Add any encryptor implemented here as follows:
+        // ExampleEncryptor Example { get; set; } = new ExampleEncryptor(SecretApplicationKey, OtherDefaultSettingsStuff);
+#pragma warning restore S125 // Sections of code should not be "commented out"
+    }
+
+}
---
+++ FileSystemBackup.Core/Encryption/IEncryptor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemBackup.Core.Encryption
+{
+    /// <summary>
+    /// Provides access to encryption methods.
+    /// </summary>
+    public interface IEncryptor
+    {
+        /// <summary>
+        /// Encrypts a given batch of data.
+        /// </summary>
+        /// <param name="arrUnencryptedData">A batch of unencrypted data as byte[].</param>
+        /// <returns>The content of <paramref name="arrUnencryptedData"/>, but encrypted.</returns>
+        byte[] Encrypt(byte[] arrUnencryptedData);
+
+        /// <summary>
+        /// Decrypts a given batch of data.
+        /// </summary>
+        /// <param name="arrEncryptedData">A batch of encrypted data as byte[].</param>
+        /// <returns>The content of <paramref name="arrEncryptedData"/>, but unencrypted.</returns>
+        byte[] Decrypt(byte[] arrEncryptedData);
+    }
+}
