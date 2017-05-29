3931a926aa70066c4f829fbf967a1804728edc16
Piet Ubrich <ubrich@inray.de>
2017-05-29 19:33:54 +0200

    Added compression

---
+++ FileSystemBackup.Core/Compression/Compression.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemBackup.Core.Compression
+{
+    /// <summary>
+    /// Provides access to all compression methods.
+    /// </summary>
+    public static class Compression
+    {
+#pragma warning disable S125 // Sections of code should not be "commented out"
+        // TODO: Add any compressor implemented here as follows:
+        // ExampleCompressor Example { get; set; } = new ExampleCompressor(CompressionSettings);
+#pragma warning restore S125 // Sections of code should not be "commented out"
+    }
+}
---
+++ FileSystemBackup.Core/Compression/ICompression.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemBackup.Core.Compression
+{
+    /// <summary>
+    /// Provides access to compression methods.
+    /// </summary>
+    public interface ICompressor
+    {
+        /// <summary>
+        /// Compresses a batch of uncompressed data into a smaller data set.
+        /// </summary>
+        /// <param name="arrUncompressedData">A batch of uncompressed data that should be compressed.</param>
+        /// <returns>The content of <paramref name="arrUncompressedData"/> in a compressed form.</returns>
+        byte[] Compress(byte[] arrUncompressedData);
+
+        /// <summary>
+        /// Decompresses a batch of compressed data into the original data set.
+        /// </summary>
+        /// <param name="arrCompressedData">A batch of compressed data that should be decompressed.</param>
+        /// <returns>The content of <paramref name="arrCompressedData"/> in an uncompressed form.</returns>
+        byte[] Decompress(byte[] arrCompressedData);
+    }
+}
